bb0b562a5104475bc8bde611c789995573fe5587
fatihoune-devs <167800900+fatihoune-devs@users.noreply.github.com>
2025-10-16 01:39:39 +0000

    update:auth-client.ts

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -5,7 +5,7 @@ import { stripeClient } from "@better-auth/stripe/client"
 import { admin, member, owner, super_admin } from "./auth/permissions";
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000",
+    baseURL: process.env.BETTER_AUTH_URL,
     plugins: [
         organizationClient({
             roles: {
